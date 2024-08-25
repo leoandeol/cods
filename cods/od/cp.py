More fixes but still bug in loss computation persisting

cods/od/cp.py
@@ -647,30 +647,34 @@ class ConfidenceConformalizer(Conformalizer):
                 verbose=True,
                 overload_confidence_threshold=1 - lbd,
             )
-            conf_boxes = list(
-                [
-                    x[y >= 1 - lbd]
-                    # rippity rip to my trick
-                    # if len(x[y >= 1 - lbd]) > 0
-                    # else x[None, y.argmax()]
-                    for x, y in zip(
-                        predictions.pred_boxes,
-                        predictions.confidence,
-                    )
-                ],
-            )
+            # for matching we always provide the full conf_boxes list
+            # conf_boxes = list(
+            #     [
+            #         x[y >= 1 - lbd]
+            #         # rippity rip to my trick
+            #         # if len(x[y >= 1 - lbd]) > 0
+            #         # else x[None, y.argmax()]
+            #         for x, y in zip(
+            #             predictions.pred_boxes,
+            #             predictions.confidence,
+            #         )
+            #     ],
+            # )
             # TODO(leoandeol): cleanify this
             # First enlarge bounding boxes to the max size
             # TODO(leoandeol): this is hardcoded, we should get input image size somewhere
             conf_boxes = apply_margins(
-                conf_boxes,
+                predictions.pred_boxes,
                 [500, 500, 500, 500],
                 mode="additive",
             )
             # Second, prediction sets for classification with always everything
             n_classes = len(predictions.pred_cls[0][0].squeeze())
             conf_cls = [
-                [[torch.arange(n_classes)] for true_cls_i_j in true_cls_i]
+                [
+                    torch.arange(n_classes)[None, ...]
+                    for true_cls_i_j in true_cls_i
+                ]
                 for true_cls_i in predictions.true_cls
             ]
 
@@ -728,7 +732,7 @@ class ConfidenceConformalizer(Conformalizer):
         predictions: ODPredictions,
         alpha: float = 0.1,
         steps: int = 13,
-        bounds: List[float] = [0, 1000],
+        bounds: List[float] = [0, 1],
         verbose: bool = True,
     ) -> Tuple[float, float]:
         """ """

cods/od/utils.py
@@ -470,17 +470,22 @@ def compute_risk_object_level(
         conf_cls_i = conf_cls[i]
         for j in range(len(true_boxes_i)):
             matching_i = predictions.matching[i]
-            matched_conf_boxes_i = list(
-                [conf_boxes_i[m] for m in matching_i[j]],
-            )
-            matched_conf_cls_i = list(
-                [conf_cls_i[m] for m in matching_i[j]],
-            )
+            if len(matching_i) == 0:
+                matched_conf_boxes_i = torch.tensor([]).cuda()
+                matched_conf_cls_i = torch.tensor([]).cuda()
+                # Unsure above
+            else:
+                matched_conf_boxes_i = torch.stack(
+                    [conf_boxes_i[m] for m in matching_i[j]],
+                )
+                matched_conf_cls_i = torch.stack(
+                    [conf_cls_i[m] for m in matching_i[j]],
+                )
             loss_value = loss(
-                true_boxes_i[j],
-                true_cls_i[j],
-                matched_conf_boxes_i,
-                matched_conf_cls_i,
+                [true_boxes_i[j]],
+                [true_cls_i[j]],
+                [matched_conf_boxes_i],
+                [matched_conf_cls_i],
             )
             losses.append(loss_value)
     losses = torch.stack(losses).ravel()
@@ -520,13 +525,13 @@ def compute_risk_image_level(
         matching_i = predictions.matching[i]
         matched_conf_boxes_i = list(
             [
-                list([conf_boxes_i[m] for m in matching_i[j]])
+                torch.stack([conf_boxes_i[m] for m in matching_i[j]])
                 for j in range(len(true_boxes_i))
             ],
         )
         matched_conf_cls_i = list(
             [
-                list([conf_cls_i[m] for m in matching_i[j]])
+                torch.stack([conf_cls_i[m] for m in matching_i[j]])
                 for j in range(len(true_boxes_i))
             ],
         )